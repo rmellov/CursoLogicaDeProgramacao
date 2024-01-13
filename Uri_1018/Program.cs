b803143416ee258362c501ef60611fa7a62e4eb8
rmellov <rmellov@gmail.com>
2024-01-13 16:40:56 -0300

    Upload exercicios

---
+++ Uri_1018/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace Uri_1018
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int valor, nota100, nota50, nota20, nota10, nota5, nota2, nota1, resto;
+
+            valor = int.Parse(Console.ReadLine());
+
+            nota100 = valor / 100;
+            resto = valor % 100;
+
+            nota50 = resto / 50;
+            resto %= 50;
+
+            nota20 = resto / 20;
+            resto %= 20;
+
+            nota10 = resto / 10;
+            resto %= 10;
+
+            nota5 = resto / 5;
+            resto %= 5;
+
+            nota2 = resto / 2;
+            resto %= 2;
+
+            nota1 = resto / 1;
+
+            Console.WriteLine(valor);
+            Console.WriteLine("{0} nota(s) de R$ 100,00", nota100);
+            Console.WriteLine("{0} nota(s) de R$ 50,00", nota50);
+            Console.WriteLine("{0} nota(s) de R$ 20,00", nota20);
+            Console.WriteLine("{0} nota(s) de R$ 10,00", nota10);
+            Console.WriteLine("{0} nota(s) de R$ 5,00", nota5);
+            Console.WriteLine("{0} nota(s) de R$ 2,00", nota2);
+            Console.WriteLine("{0} nota(s) de R$ 1,00", nota1);
+        }
+    }
+}
---
+++ Uri_1020/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace Uri_1020
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int idadeEmDias, anos, meses, dias, resto;
+
+            idadeEmDias = int.Parse(Console.ReadLine());
+
+            anos = idadeEmDias / 365;
+            resto = idadeEmDias % 365;
+
+            meses = resto / 30;
+            dias = resto % 30;
+
+            Console.WriteLine("{0} ano(s)", anos);
+            Console.WriteLine("{0} mes(es)", meses);
+            Console.WriteLine("{0} dia(s)", dias);
+        }
+    }
+}
