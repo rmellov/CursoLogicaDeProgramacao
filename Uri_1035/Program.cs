4f4d8e94130b730199d18b2c58953d4570913508
rmellov <rmellov@gmail.com>
2024-01-17 09:05:37 -0300

    Upload exercicio

---
+++ Uri_1035/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Uri_1035
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, b, c, d;
+            string[] vet = Console.ReadLine().Split(' ');
+
+            a = int.Parse(vet[0]);
+            b = int.Parse(vet[1]);
+            c = int.Parse(vet[2]);
+            d = int.Parse(vet[3]);
+
+            if (b > c && d > a && c + d > a + b && c > 0 && d > 0 && a % 2 == 0)
+                Console.WriteLine("Valores aceitos");
+            else
+                Console.WriteLine("Valores nao aceitos");
+        }
+    }
+}
