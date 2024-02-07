b7f8b212520923dbde21e9a498c20ce8216514ba
rmellov <rmellov@gmail.com>
2024-02-06 21:33:15 -0300

    Upload exercicios

---
+++ Uri_1114/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace Uri_1114
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string senha = Console.ReadLine();
+
+            while (!senha.Equals("2002"))
+            {
+                Console.WriteLine("Senha Invalida");
+                senha = Console.ReadLine();
+            }
+
+            Console.WriteLine("Acesso Permitido");
+        }
+    }
+}
